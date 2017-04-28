08733d44ab75ef0c41818862db164a0a0a546501
Gregory V. Bilello <gregory.bilello@gmail.com>
2017-04-28 11:01:04 -0400

    Add Feed scaffold, overwrite earlier changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  resources :feeds
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :feeds, only: [:index, :show]
 
   root 'feeds#index'
 end
